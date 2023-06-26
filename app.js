abec81da2e589da8bd0a4963ef2ff0232cdaabd4
jck-bit <jckkinyanjui@gmail.com>
2023-06-26 21:42:10 +0300

    rewrote the whole about-me page

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
-const contents = document.querySelectorAll('.content');
-contents.forEach(content => {
-  content.addEventListener('transitionend', () => {
-    if (content.classList.contains('content-exit')) {
-      content.style.display = 'none';
-    }
-  });
-});
-
-const links = document.querySelectorAll('.navbar a');
-
-links.forEach(link => {
-link.addEventListener('click', e => {
-    e.preventDefault();
-
-    const targetId = link.getAttribute('href');
-    const targetElem = document.querySelector(targetId);
-    const targetOffsetTop = targetElem.getBoundingClientRect().top + window.pageYOffset;
-
-    document.body.classList.add('animate');
-
-    window.setTimeout(() => {
-    document.body.classList.remove('animate');
-    window.scrollTo({
-        top: targetOffsetTop,
-        behavior: 'smooth'
+window.addEventListener("load", function() {
+    document.querySelectorAll(".transition-fade, .transition-came, .transition-right").forEach(function(element) {
+      element.classList.add("active");
     });
-    }, 1000);
-});
-})
+  });
+
+
+//fetch github repos
+let repositoryData = null;
+
+async function fetchRepositories() {
+  if (repositoryData) {
+    // If repository data is already available, use it directly
+    renderRepositories(repositoryData);
+    return;
+  }
+
+  const username = 'jck-bit';
+  const apiUrl = `https://api.github.com/users/${username}/repos`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    repositoryData = data; // Cache the repository data
+    renderRepositories(repositoryData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function renderRepositories(data) {
+  const repositoryList = document.getElementById('repository-list');
+
+  data.forEach(repo => {
+    const column = document.createElement('div');
+    column.classList.add('col-md-4', 'mb-4');
+
+    const card = document.createElement('div');
+    card.classList.add('card', 'h-100');
+    card.innerHTML = `
+      <div class="card-header">
+        <i class="fa fa-github"></i>
+        <p class="m-0">${repo.name}</p>
+      </div>
+      <div class="card-body">
+        <p class="card-text">${repo.description}</p>
+      </div>
+      <div class="card-footer d-flex  align-items-center ">
+        <span class="card-text">${repo.language}</span>
+        <span><i class="fa fa-star"></i> Stars: ${repo.stargazers_count}</span>
+        <span><i class="fa fa-code-fork"></i> Forks: ${repo.forks_count}</span>
+      </div>
+    `;
+
+    column.appendChild(card);
+    repositoryList.appendChild(column);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', fetchRepositories);
