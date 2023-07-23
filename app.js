2d3191de00bc3ef85cf951f327466ac204d87260
jck-bit <jckkinyanjui@gmail.com>
2023-07-24 00:00:06 +0300

    removed unwanted comments

--- app.js
+++ app.js
@@ -4,11 +4,3 @@ window.addEventListener("load", function() {
     });
   });
 
-
-//if the location  of the pathname is connect, then the display of the footer should be none
-
-
-
-
-//add an event-listener on the nav items, if its clicked and the location of the page is not connect.html, the footer display is none
-
